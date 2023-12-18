ea6cfc9d7ac4354f2161854cc423ceccb0122373
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-12-18 22:15:18 +0000

    finished generic types

--- basics/merging-types/literal-types.ts
+++ basics/merging-types/literal-types.ts
@@ -19,3 +19,49 @@ const performAction = (action: string, role: Role) => {
 
 performAction('delete', 'admin')
 performAction('create', 'user')
+
+// let roles = Role[]
+let roles: Array<Role>;
+
+roles = ['admin', 'user', 'editor', 'admin']
+
+type DataStorage<T> = {
+    storage: T[];
+    add: (data: T) => void;
+}
+
+const textStorage: DataStorage<string> = {
+    storage: [],
+    add: (data: string) => {
+        textStorage.storage.push(data)
+    }
+}
+
+const userStorage: DataStorage<{ name: string }> = { 
+    storage: [],
+    add: (user: { name: string }) => {
+        userStorage.storage.push(user)
+    }
+
+}
+
+// function merge<T, U>(a: T, b: U) {
+//     return {
+//         ...a,
+//         ...b,
+//     };
+// }
+
+const merge = <T, U>(a: T, b: U) => {
+    return {
+        ...a,
+        ...b,
+    };
+}
+
+const user = merge<{ name: string }, { age: number }>(
+    { name: 'Max' },
+    { age: 30 }
+);
+
+console.log(user)
