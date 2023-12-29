74f81da73e13a6f85d82206b5a6ee8b1d79ae34b
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-12-29 15:59:04 +0000

    use state for addcoarsegoal funmction

--- TypescriptWithReact/Project/react-ts-basics/src/App.tsx
+++ TypescriptWithReact/Project/react-ts-basics/src/App.tsx
@@ -1,11 +1,45 @@
-import './index.css'
-import CourseGoal from './components/CourseGoals';
-const App = ()  => {
-  return <main>
-    <CourseGoal title='Goal 1'>
-      <p>This is the description of the course goal</p>
-    </CourseGoal>
-  </main>
-}
+import "./index.css";
+import CourseGoal from "./components/CourseGoals";
+import Header from "./components/Header";
+import GoalsImg from "./assets/goals.jpg";
+import { useState } from "react";
 
-export default App
+type CoarseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
+const App = () => {
+  const [goals, setGoals] = useState<CoarseGoal[]>([]);
+
+  const addGoalHandler = () => {
+    setGoals((prevGoals) => {
+      const newGoal: CoarseGoal = {
+        id: Math.random(),
+        title: "My new goal",
+        description: "My new description in depth",
+      };
+      return [...prevGoals, newGoal];
+    });
+  };
+  return (
+    <main>
+      <div>
+        <Header image={GoalsImg}>Course Goals</Header>
+        <button onClick={addGoalHandler}>Click Me</button>
+        {goals.map((goal) => (
+          <ul>
+            <li key={goal.id}>
+              <CourseGoal title={goal.title}>
+                <p>{goal.description}</p>
+              </CourseGoal>
+            </li>
+          </ul>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default App;
--- TypescriptWithReact/Project/react-ts-basics/src/components/CourseGoals.tsx
+++ TypescriptWithReact/Project/react-ts-basics/src/components/CourseGoals.tsx
@@ -1,7 +1,4 @@
 import { type ReactNode } from "react"
-import Header from "./Header"
-import GoalsImg from '../assets/goals.jpg'
-
 type CourseGoalProps = {
     title: string,
     children: ReactNode
@@ -12,9 +9,6 @@ const CourseGoal = ({title, children} : CourseGoalProps) => {
 
     return <article>
         <div>
-            <Header image={GoalsImg}>
-                Course Goals
-            </Header>
             <h2>{title}</h2>
             <div>
                 {children}
