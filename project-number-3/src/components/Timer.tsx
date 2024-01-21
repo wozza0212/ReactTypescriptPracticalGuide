91a03a9344ddff71f8ccd81601d9cb183d3520d4
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2024-01-21 22:04:00 +0000

    needs fix, timers no stopping

--- project-number-3/src/components/Timer.tsx
+++ project-number-3/src/components/Timer.tsx
@@ -1,5 +1,6 @@
 import Container from './UI/Container.tsx';
-import {type Timer as TimerProps } from '../store/timers-context.tsx';
+import {useTimersContext, type Timer as TimerProps } from '../store/timers-context.tsx';
+import { useEffect, useRef, useState } from 'react';
 
 // type TimerProps = {
 //   name: string;
@@ -7,10 +8,35 @@ import {type Timer as TimerProps } from '../store/timers-context.tsx';
 // }
 
 const Timer = ({name, duration}: TimerProps) => {
+  const interval = useRef<number | null>(null)
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const {isRunning} = useTimersContext()
+
+  if(remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current)
+  }
+  useEffect(() => {
+    let timer: number;
+    if(isRunning) {
+    const timer = setInterval(() => {
+      setRemainingTime(prevTime => prevTime - 50)
+    }, 50)
+    interval.current = timer
+  }else if(interval.current) {
+    clearInterval(interval.current)
+  }
+
+
+    return () => {clearInterval(timer)}
+  }, [isRunning])
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+  
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p><progress max={duration * 1000} value={remainingTime}/></p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
