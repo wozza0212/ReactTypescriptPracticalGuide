06e694bf5313c84414fc58197fa96d46efb9c05c
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2024-01-27 21:49:15 +0000

    add and implement data fetching util

--- project-4/src/App.tsx
+++ project-4/src/App.tsx
@@ -1,4 +1,25 @@
+import { useState, useEffect } from "react";
+import get from "./utils/http";
+import { BlogPost } from "./components/BlogPosts";
+
+type RawDataBlogPost = {
+  id: number;
+  title: string;
+  body: string;
+};
+
 function App() {
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[] | undefined>();
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const data = (await get(
+        "https://jsonplaceholder.typicode.com/posts"
+      )) as RawDataBlogPost[];
+    };
+    
+    fetchPosts();
+  }, []);
+
   return <h1>Data Fetching!</h1>;
 }
 
